dd3161038a5883e80dbadc310a864fd3205b8f1d
Emilio <em.francomartinez@gmail.com>
2019-01-03 16:27:43 -0700

    created login and register

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React, { Fragment } from "react";
 import Home from "./components/Home";
 import NoMatch from "./components/NoMatch";
 import Navbar from "./components/NavBar";
-// import Login from "./components/Register";
+import Login from "./components/Login";
 import { Switch, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
-import { register } from "./serviceWorker";
+import Register from "./components/Register";
 
 const App = () => (
   <Fragment>
@@ -13,8 +13,8 @@ const App = () => (
     <Container>
       <Switch>
         <Route exact path="/" component={Home} />
-        {/* <Route exact path="/login" component={Login} /> */}
-        {/* <Route exact path="/register" component={Register} /> */}
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
